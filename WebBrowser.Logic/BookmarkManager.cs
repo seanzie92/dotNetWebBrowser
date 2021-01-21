d091d7c3a721a70dc46e74d8f751831b739161ab
seanzie92 <seanzetterlund@gmail.com>
2021-01-21 13:55:48 -0800

    added more methods

--- WebBrowser.Logic/BookmarkManager.cs
+++ WebBrowser.Logic/BookmarkManager.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace WebBrowser.Logic
 {
-    class BookmarkManager
+    //we need methods for adding bookmark items, getting them, deleting them etc
+    public class BookmarkManager
     {
+        //add bookmark items
+        public static void AddItem(BookmarkItem item)
+        {
+            var add //will implement in future module,  but there should be a 
+                    //var declared and a method for inserting bookmarks
+        }
+
+        //gets and shows bookmark items
+        public static List<BookmarkItem> GetItems()
+        {
+            var items.. //will implement in future module, but there should be a 
+                        //for loop that iterates through the items and return them
+        }
+
+        //delete bookmark items
+        public static void DeleteBook()
+        {
+            var item.. //will implement in future module, but we need a for loop then
+                       // maybe an if state to find a specific item then use a delete method
+        }
     }
 }
