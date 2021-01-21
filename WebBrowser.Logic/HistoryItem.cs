e342316fc38541404db242506d7bbce66c42e4bf
seanzie92 <seanzetterlund@gmail.com>
2021-01-21 13:20:55 -0800

    getters and setters added

--- WebBrowser.Logic/HistoryItem.cs
+++ WebBrowser.Logic/HistoryItem.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace WebBrowser.Logic
 {
-    class HistoryItem
+    //get and set methods for history 
+    public class HistoryItem
     {
+        public int Id
+            {get; set;}
+
+        public string URL
+            {get; set;}
+
+        public string Title
+            {get; set;}
+
+        public DateTime Date
+            {get; set;}
     }
 }
