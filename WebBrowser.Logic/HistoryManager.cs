df066a11c46f860894ee053573cbb6be1bac901e
seanzie92 <seanzetterlund@gmail.com>
2021-01-21 13:42:57 -0800

    outlined methods for HistoryManager

--- WebBrowser.Logic/HistoryManager.cs
+++ WebBrowser.Logic/HistoryManager.cs
@@ -4,8 +4,34 @@ using System.Text;
 
 namespace WebBrowser.Logic
 {
+    //we need methods for adding history, getting it, deleting all of it, and deleting specifics
     public class HistoryManager
     {
+        //add items
+        public static void AddItem(HistoryItem item)
+        {
+            var items.. //will implement in future module, but this should update and add item
+        }
 
+        //retrieves and gets history 
+        public static List<HistoryItem> GetItem()
+        {
+            var items //will implement in future module, but there should be varibales declared
+                      // there should be a for loop to go through items and they should all be returned
+        }
+
+        //deletes history 
+        public static void DeleteHistory()
+        {
+            var rows //will implement more in future module, but after var declaration, we will
+                     //need another for loop to find and delete 
+        }
+
+        //removes history
+        public static void RemoveHistory()
+        {
+            var row //will implement in future module, but we'll need a for loop to look for a specific 
+                    //item and delete it. Probably will need a string that'll check rows too.
+        }
     }
 }
