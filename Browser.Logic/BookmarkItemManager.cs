b67a4e4a54019b8e0821f85b37f90a56dc577c39
seanzie92 <seanzetterlund@gmail.com>
2021-02-11 13:26:37 -0800

    Added the Manager classes for Bookmark and History

---
+++ Browser.Logic/BookmarkItemManager.cs
@@ -0,0 +1,31 @@
+﻿using Browser.Data.BookmarkDataTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Browser.Logic
+{
+    class BookmarkItemManager
+    {
+        public void AddBookmarkItem(BookmarkItem item)
+        {
+            var adapter = new BookmarkTableAdapter();
+            adapter.Insert(item.URL, item.Title);
+        }
+
+        public List<BookmarkItem> GetItems()
+        {
+            var result = new List<BookmarkItem>();
+            var adapter = new BookmarkTableAdapter();
+
+            foreach (var row in adapter.GetData())
+            {
+                result.Add(new BookmarkItem() { URL = row.URL, Title = row.Title });
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Browser.Logic/HistoryItemManager.cs
@@ -0,0 +1,32 @@
+﻿using Browser.Data.HistoryDataTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+
+namespace Browser.Logic
+{
+    class HistoryItemManager
+    {
+        public List<HistoryItem> GetItems()
+        {
+            var result = new List<HistoryItem>();
+            var adapter = new HistoryTableAdapter();
+
+            foreach(var row in adapter.GetData())
+            {
+                result.Add(new HistoryItem() { URL = row.URL, Title = row.Title, Date = row.Date });
+            }
+
+            return result;
+        }
+
+        public void AddHistoryItem(HistoryItem item)
+        {
+            var adapter = new HistoryTableAdapter();
+            adapter.Insert(item.URL, item.Title, item.Date);
+        }
+    }
+}
