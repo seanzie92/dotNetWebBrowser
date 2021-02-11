f80415ba6ad0d166ecd03450fe6f13e46d21f8e1
seanzie92 <seanzetterlund@gmail.com>
2021-02-11 12:23:48 -0800

    Added item classes

---
+++ Browser.Logic/BookmarkItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Browser.Logic
+{
+    public class BookmarkItem
+    {
+        public int Id { get; set; }
+        public string URL { get; set; }
+        public string Title { get; set; }
+    }
+}
