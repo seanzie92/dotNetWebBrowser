bb7e78a808932a79c3309dded8884765b979177f
seanzie92 <seanzetterlund@gmail.com>
2021-02-16 12:19:40 -0800

    changed properties to buttons

--- WebBrowser.UI/HistoryForm.Designer.cs
+++ WebBrowser.UI/HistoryForm.Designer.cs
@@ -30,10 +30,10 @@ namespace WebBrowser.UI
         private void InitializeComponent()
         {
             this.historyListBox = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.histSearchBox = new System.Windows.Forms.TextBox();
+            this.histSearchButton = new System.Windows.Forms.Button();
+            this.histDeleteButton = new System.Windows.Forms.Button();
+            this.histClearButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // historyListBox
@@ -45,50 +45,50 @@ namespace WebBrowser.UI
             this.historyListBox.Size = new System.Drawing.Size(482, 304);
             this.historyListBox.TabIndex = 0;
             // 
-            // textBox1
+            // histSearchBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(108, 82);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(407, 47);
-            this.textBox1.TabIndex = 1;
+            this.histSearchBox.Location = new System.Drawing.Point(108, 82);
+            this.histSearchBox.Multiline = true;
+            this.histSearchBox.Name = "histSearchBox";
+            this.histSearchBox.Size = new System.Drawing.Size(407, 47);
+            this.histSearchBox.TabIndex = 1;
             // 
-            // button1
+            // histSearchButton
             // 
-            this.button1.Location = new System.Drawing.Point(108, 214);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 41);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Search";
-            this.button1.UseVisualStyleBackColor = true;
+            this.histSearchButton.Location = new System.Drawing.Point(108, 214);
+            this.histSearchButton.Name = "histSearchButton";
+            this.histSearchButton.Size = new System.Drawing.Size(75, 41);
+            this.histSearchButton.TabIndex = 2;
+            this.histSearchButton.Text = "Search";
+            this.histSearchButton.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // histDeleteButton
             // 
-            this.button2.Location = new System.Drawing.Point(108, 343);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 38);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Delete";
-            this.button2.UseVisualStyleBackColor = true;
+            this.histDeleteButton.Location = new System.Drawing.Point(108, 343);
+            this.histDeleteButton.Name = "histDeleteButton";
+            this.histDeleteButton.Size = new System.Drawing.Size(75, 38);
+            this.histDeleteButton.TabIndex = 3;
+            this.histDeleteButton.Text = "Delete";
+            this.histDeleteButton.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // histClearButton
             // 
-            this.button3.Location = new System.Drawing.Point(108, 480);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 38);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Clear";
-            this.button3.UseVisualStyleBackColor = true;
+            this.histClearButton.Location = new System.Drawing.Point(108, 480);
+            this.histClearButton.Name = "histClearButton";
+            this.histClearButton.Size = new System.Drawing.Size(75, 38);
+            this.histClearButton.TabIndex = 4;
+            this.histClearButton.Text = "Clear";
+            this.histClearButton.UseVisualStyleBackColor = true;
             // 
             // HistoryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1043, 590);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.histClearButton);
+            this.Controls.Add(this.histDeleteButton);
+            this.Controls.Add(this.histSearchButton);
+            this.Controls.Add(this.histSearchBox);
             this.Controls.Add(this.historyListBox);
             this.Name = "HistoryForm";
             this.Text = "HistoryForm";
@@ -101,9 +101,9 @@ namespace WebBrowser.UI
         #endregion
 
         private System.Windows.Forms.ListBox historyListBox;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox histSearchBox;
+        private System.Windows.Forms.Button histSearchButton;
+        private System.Windows.Forms.Button histDeleteButton;
+        private System.Windows.Forms.Button histClearButton;
     }
 }
